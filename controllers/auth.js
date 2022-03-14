6565000780fb7aa62650695910ad15f92075be89
EdwardR0507 <ramosedward21@gmail.com>
2022-03-13 20:33:50 -0500

    feat: models

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,43 @@
-const authCreateUser = (req, res) => {
+const bcrypt = require("bcryptjs");
+const User = require("../models/User");
+
+const authCreateUser = async (req, res) => {
   const { name, email, password } = req.body;
+
+  try {
+    let user = await User.findOne({ email });
+    if (user) {
+      return res.status(400).json({
+        ok: false,
+        message: "User already exists with this email",
+      });
+    }
+
+    user = new User({
+      name,
+      email,
+      password,
+    });
+
+    // Encrypt password
+    const salt = bcrypt.genSaltSync();
+    user.password = bcrypt.hashSync(password, salt);
+
+    await user.save();
+
+    res.status(201).json({
+      ok: true,
+      message: "Successful registration",
+      uid: user._id,
+      name: user.name,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      message: "Communicate with the administrator",
+    });
+  }
 
   res.status(201).json({
     ok: true,
