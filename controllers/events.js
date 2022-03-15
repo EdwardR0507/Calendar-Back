e1ca10a9cd7f74778fa36ad9555855fb5a6a6014
EdwardR0507 <ramosedward21@gmail.com>
2022-03-14 22:56:19 -0500

    feat: update event

--- controllers/events.js
+++ controllers/events.js
@@ -30,16 +30,46 @@ const eventsCreateEvent = async (req, res) => {
     console.log(error);
     res.status(500).json({
       ok: false,
-      message: "Error creating event",
+      message: "Communicate with the administrator",
     });
   }
 };
 
-const eventsUpdateEvent = (req, res) => {
-  res.json({
-    ok: true,
-    message: "Update event",
-  });
+const eventsUpdateEvent = async (req, res) => {
+  const { id } = req.params;
+  const { body, uid } = req;
+  try {
+    const event = await Event.findById(id);
+    if (!event) {
+      return res.status(404).json({
+        ok: false,
+        message: "Event not found",
+      });
+    }
+    if (event.user.toString() !== uid) {
+      return res.status(401).json({
+        ok: false,
+        message: "Not authorized",
+      });
+    }
+    const newEvent = {
+      ...body,
+      user: uid,
+    };
+    const eventUpdated = await Event.findByIdAndUpdate(id, newEvent, {
+      new: true,
+    });
+    res.status(200).json({
+      ok: true,
+      event: eventUpdated,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({
+      ok: false,
+      message: "Communicate with the administrator",
+    });
+  }
 };
 
 const eventsDeleteEvent = (req, res) => {
